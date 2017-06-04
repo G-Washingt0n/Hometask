4be1658960212065f0188fef9443ec3bd5670a17
George <martsulg.p@gmail.com>
2017-06-05 02:24:30 +0300

    банкомат выдает и принимает деньги

---
+++ DZ6_ATM/src/dz6_atm/Money.java
@@ -0,0 +1,11 @@
+package dz6_atm;
+
+
+public class Money {
+    public static int countMoney(int a1, int a2, int b1, int b2, int c1, int c2){
+    int summ;
+    summ= a1*a2 + b1*b2 + c1*c2;
+    return summ;
+}
+    
+}
