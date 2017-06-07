c5f23c8bc658d08149200f1dabb714d8062144c2
George <martsulg.p@gmail.com>
2017-06-07 18:37:05 +0300

    добавлен интерфейс

---
+++ DZ6_ATM/src/dz6_atm/ActionListener.java
@@ -0,0 +1,7 @@
+package dz6_atm;
+
+public interface ActionListener {
+    
+    public void actionGet(int a);
+    
+}
--- DZ6_ATM/src/dz6_atm/Money.java
+++ DZ6_ATM/src/dz6_atm/Money.java
@@ -2,10 +2,21 @@ package dz6_atm;
 
 
 public class Money {
+    private ActionListener listener;
+
+    public void setListener(ActionListener listener) {
+        this.listener = listener;
+    }
+    public void toGet(int a){
+        listener.actionGet(a);
+    }
+    
+    
     public static int countMoney(int a1, int a2, int b1, int b2, int c1, int c2){
     int summ;
     summ= a1*a2 + b1*b2 + c1*c2;
     return summ;
 }
+    
     
 }
