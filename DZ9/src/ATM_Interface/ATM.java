67b1eb498e47adaa16d3e02a5db6c982e2be36b2
George <martsulg.p@gmail.com>
2017-06-14 00:29:40 +0300

    ATM with anonimus

---
+++ DZ9/src/ATM_Interface/ATM.java
@@ -0,0 +1,67 @@
+package ATM_Interface;
+
+import java.util.Scanner;
+
+public class ATM {
+   
+    
+    public void main() {
+         Money money = new Money();        
+        money.setListener(new ActionListener() {
+             @Override
+             public void actionGet(int[] Comb) {  
+                 System.out.println("Операция прошла успешно\n" + "Комбинация купюр: " + "100x" + Comb[0] + " 50x" + Comb[1] + " 20x" + Comb[2]);
+             }
+
+             @Override
+             public void actionAdd() {
+                 System.out.println("Деньги добавлены в банкомат");
+             }
+         });
+        int operationType = 0;
+
+        while (operationType != 3) {
+
+            System.out.println("Банкомат. для снятия денег нажмите 1, для добавления денег в банкомат нажмите 2, для завершения работы нажмите 3:");
+            Scanner in = new Scanner(System.in);
+            operationType = in.nextInt();
+
+            switch (operationType) {
+                case 1:
+                    System.out.println("Какую сумму вы хотите снять? ");
+                    int summOut = in.nextInt();
+                    money.toGet(summOut);
+                    //getCash(summOut);
+                    System.out.println("Для продолжения нажмите 1, для выхода нажмите 2: ");
+                    if (in.nextInt() == 2) {
+                        operationType = 3;
+                    }
+                    break;
+                case 2:
+                    money.toAdd();
+                    //addCash();
+                    System.out.println("Для продолжения нажмите 1, для выхода нажмите 2: ");
+                    if (in.nextInt() == 2) {
+                        operationType = 3;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Всего доброго!");
+                    break;
+                default:
+                    System.out.println("Ошибка операции! Повторите ввод!");
+                    break;
+            }
+
+        }
+    }
+
+    public void actionGet(int[] Comb) {
+        System.out.println("Операция прошла успешно\n" + "Комбинация купюр: " + "100x" + Comb[0] + " 50x" + Comb[1] + " 20x" + Comb[2]);
+    }
+
+    public void actionAdd() {
+        System.out.println("Деньги добавлены в банкомат");
+    }
+
+}
---
+++ DZ9/src/ATM_Interface/Banknote20.java
@@ -0,0 +1,23 @@
+package ATM_Interface;
+
+
+public class Banknote20 {
+    private final int value = 20;
+    private int amount = 5; //изначально в банкомате будет только 5 банкнот номиналом 20
+
+    public int getValue() {
+        return value;
+    }
+
+    public Banknote20() {
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+}
